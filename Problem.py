39d149e485d9a0a146a52a5651968fe3835b3eb7
Will Seymour <willseymour804@gmail.com>
2022-08-02 16:00:34 -0500

    initial

---
+++ Problem.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+from enum import Enum
+from queue import PriorityQueue
+
+class Problem:
+
+    def __init__(self, initial_state: list):
+        # The set of explored states
+        self.explored = set()
+        # Uniform step cost for this problem
+        self._STEPCOST = 1
+        # The frontier of states to explore order by path cost
+        self.frontier = PriorityQueue()
+
+    class Action(Enum):
+        """ Enumeration of actions."""
+        UP = 'UP'
+        DOWN = 'DOWN'
+        LEFT = 'LEFT'
+        RIGHT = 'RIGHT'
+
+    class Node:
+
+        def __init__(self, state: list, parent: Problem.Node, action: Problem.Action, pathCost: int):
+             # The state represented by the node
+            self.state = state
+            # The parent node that preceded this state
+            self.parent = parent
+            # The action that was taken from the parent state to get to this state
+            self.action = action
+            # The path cost of achieving this state from the initial state
+            self.pathCost = pathCost
+    
+    def actions(self, state: list) -> list:
+        """ Returns a list of potential actions from a given state."""
+        blank = state.index(0)
+
+        if blank == 0:
+            return [ self.Action.RIGHT, self.Action.DOWN ]
+        elif blank == 1:
+            return [ self.Action.LEFT, self.Action.RIGHT, self.Action.DOWN ]
+        elif blank == 2:
+            return [ self.Action.LEFT, self.Action.DOWN ]
+        elif blank == 3:
+            return [ self.Action.DOWN, self.Action.UP, self.Action.RIGHT ]
+        elif blank == 4:
+            return [ self.Action.DOWN, self.Action.UP, self.Action.RIGHT, self.Action.LEFT ]    
+        elif blank == 5:
+            return [ self.Action.LEFT, self.Action.UP, self.Action.DOWN ]
+        elif blank == 6:
+            return [ self.Action.UP, self.Action.RIGHT ]
+        elif blank == 7:
+            return [ self.Action.LEFT, self.Action.RIGHT, self.Action.UP ]
+        elif blank == 8:
+            return [ self.Action.LEFT, self.Action.UP ]
+        else:
+            raise Exception('State index error.')
+
+    def heuristic(self, state: list) -> int:
+        """ 
+            Returns a heuristic measure of a state's undesirability given by the sum of the 
+            direct step distances between each block and it's goal location.
+        """
+        mdist = 0
+        from mdist import manhattan_distance
+
+        for e in state:
+            location = state.index(e)        
+            # Any given block's home index is 1 less than itself.
+            home = e - 1
+            mdist += manhattan_distance(location, home)
+        
+        return mdist
+
+
+    def create_child(self, parent: Node, action: Action):
+        """ Create a child node from the parent and an action."""        
+        pass
---
+++ mdist.py
@@ -0,0 +1,73 @@
+def manhattan_distance(self, location: int, home: int) -> int:
+        """ Returns the direct step distance between two locations on the 8-Block board."""
+        if location == home:
+            return 0
+        elif location == 0:
+            if home in [1, 3]:
+                return 1
+            elif home in [2, 4, 6]:
+                return 2
+            elif home in  [5, 7]:
+                return 3
+            else:
+                return 4
+        elif location == 1:
+            if home in [0, 2, 4]:
+                return 1
+            elif home in [3, 5, 7]:
+                return 2
+            else:
+                return 3
+        elif location == 2:
+            if home in [1, 5]:
+                return 1
+            elif home in [0, 4, 8]:
+                return 2
+            elif home in [3, 7]:
+                return 3
+            else:
+                return 4
+        elif location == 3:
+            if home in [0, 4, 6]:
+                return 1
+            elif home in [1, 5, 7]:
+                return 2
+            else:
+                return 3
+        elif location == 4:
+            if home in [1, 3, 5, 7]:
+                return 1
+            else:
+                return 2
+        elif location == 5:
+            if home in [2, 4, 8]:
+                return 1
+            elif home in [1, 3, 7]:
+                return 2
+            else:
+                return 3
+        elif location == 6:
+            if home in [3, 7]:
+                return 1
+            elif home in [0, 4, 8]:
+                return 2
+            elif home in [1, 5]:
+                return 3
+            else:
+                return 4
+        elif location == 7:
+            if home in [4, 6, 8]:
+                return 1
+            elif home in [1, 3, 5]:
+                return 2
+            else:
+                return 3
+        elif location == 8:
+            if home in [5, 7]:
+                return 1
+            elif home in [2, 4, 6]:
+                return 2
+            elif home in [1, 3]:
+                return 3
+            else:
+                return 4
