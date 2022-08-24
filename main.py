34439eb343cf213e3bee2a014c576afdc843c1dc
Will Seymour <willseymour804@gmail.com>
2022-08-23 21:08:54 -0500

    implemented manhattan distance correctly

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from Problem import Problem as P
 
-def solve(initial = [1, 2, 3, 4, 5, 6, 7, 0, 8]):
-    p = P(initial)
+def solve(initial = [1, 2, 3, 4, 5, 6, 7, 0, 8], h = None):
+    p = P(initial, h)
     root = p.Node(p.initial_state, None, None, 0)
     p.frontier.push((root.pathCost, root))
 
     while (p.frontier.size() > 0):
         # Get the next frontier node
         node = p.frontier.pop()
-        print("Popped from frontier: ", node)
+        # print("Popped from frontier: ", node)
         
-        print("Goal Test: {}".format(p.goal_test(node.state)))
+        # print("Goal Test: {}".format(p.goal_test(node.state)))
         if p.goal_test(node.state):
             return p.solution(node)
 
@@ -18,14 +18,14 @@ def solve(initial = [1, 2, 3, 4, 5, 6, 7, 0, 8]):
 
         for action in p.actions(node.state):
             child = p.create_child(node, action)
-            print(action, child)
+            # print(action, child)
 
             if (child not in p.explored) and (child.state not in p.frontier.array):
-                print('Pushed to frontier.')
+                # print('Pushed to frontier.')
                 p.frontier.push((child.pathCost, child))
             elif (child.state in p.frontier.array):
                 # Only replaces the entry if pathCost is higher
-                print('Replaced...?')
+                # print('Replaced...?')
                 p.frontier.replace(child)
         
 
